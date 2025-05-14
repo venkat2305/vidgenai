eeb958443784594de5fc169c9afc9ebcc88c3b61
venkat2305 <heliobvsr2002@gmail.com>
2025-05-14 17:55:33 +0530

    digital ocean backend

--- frontend/src/config/api-config.ts
+++ frontend/src/config/api-config.ts
@@ -2,7 +2,8 @@
 export const LOCAL_BACKEND_URL = "http://0.0.0.0:8000";
 export const PRODUCTION_BACKEND_URL_APP = "https://vidgenai-qp38.onrender.com";
 export const PRODUCTION_BACKEND_URL_DOCKER = "https://vidgenai-new.onrender.com";
-export const PRODUCTION_BACKEND_URL = PRODUCTION_BACKEND_URL_DOCKER
+export const PRODUCTION_BACKEND_URL_DIGITAL_OCEAN = "https://vidgenai-new.onrender.com";
+export const PRODUCTION_BACKEND_URL = PRODUCTION_BACKEND_URL_DIGITAL_OCEAN
 
 // MANUAL TOGGLE: Set to true to use production backend, false to use local
 export const USE_PRODUCTION_BACKEND = true;
